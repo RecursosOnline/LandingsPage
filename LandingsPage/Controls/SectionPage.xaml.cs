2a4511c11bbdae80d4e04e7731de4d248dda4029
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-04-29 16:13:03 +0330

    Update SectionPage.xaml.cs

--- LandingsPage/Controls/SectionPage.xaml.cs
+++ LandingsPage/Controls/SectionPage.xaml.cs
@@ -24,8 +24,6 @@ public sealed partial class SectionPage : ItemsPageBase
 
         if (group != null)
         {
-            var menuItem = (Microsoft.UI.Xaml.Controls.NavigationViewItemBase) navigationView.MenuItems.Single(i => (string) ((Microsoft.UI.Xaml.Controls.NavigationViewItemBase) i).Tag == group.UniqueId);
-            menuItem.IsSelected = true;
             TitleTxt.Text = group.Title;
             Items = group.Items.Where(i => !i.HideItem).OrderBy(i => i.Title).ToList();
         }
